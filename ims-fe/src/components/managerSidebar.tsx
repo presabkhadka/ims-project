21e767e31603a5f0e725333d8f2627c287ff1bf9
Presab Khadka <presabkhadka30@gmail.com>
2025-04-11 11:25:17 +0545

    feat - manager sidebar

---
+++ ims-fe/src/components/managerSidebar.tsx
@@ -0,0 +1,43 @@
+import {
+    LayoutDashboard,
+    Package,
+    PlusCircle,
+    PackageSearch,
+    ShoppingCart,
+    Truck,
+  } from "lucide-react";
+  import { NavLink } from "react-router-dom";
+  
+  export default function ManagerSidebar() {
+    const menuItems = [
+      { to: "/manager/dashboard", icon: LayoutDashboard, label: "Dashboard" },
+      { to: "/manager/products", icon: Package, label: "All Products" },
+      { to: "/manager/add-product", icon: PlusCircle, label: "Add New" },
+      { to: "/manager/stocks", icon: PackageSearch, label: "Stocks" },
+      { to: "/manager/suppliers", icon: Truck, label: "Suppliers" },
+      { to: "/manager/add-suppliers", icon: Truck, label: "Add New Supplier" },
+      { to: "/manager/orders", icon: ShoppingCart, label: "Orders" },
+    ];
+  
+    return (
+      <div className="flex flex-col gap-4 h-full border-r bg-white p-4 w-64 dark:bg-black">
+        {menuItems.map((item) => (
+          <NavLink
+            key={item.to}
+            to={item.to}
+            className={({ isActive }) =>
+              `flex items-center p-3 rounded-lg gap-3 transition-all duration-200 ${
+                isActive
+                  ? "bg-blue-50 text-blue-600 border-blue-200 font-semibold dark:border-blue-500 dark:bg-muted/80"
+                  : "text-gray-600 hover:bg-gray-50 hover:text-blue-600 dark:hover:bg-muted/80"
+              }`
+            }
+          >
+            <item.icon size={20} />
+            <span className="text-sm font-medium">{item.label}</span>
+          </NavLink>
+        ))}
+      </div>
+    );
+  }
+  
